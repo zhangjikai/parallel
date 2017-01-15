1a7906dba8e1d2c3fdc3b537ac93d092a300cd71
zhangjikai <zhangjk1993@gmail.com>
2017-01-15 20:12:20 +0800

    update

--- parallel.sh
+++ parallel.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
-cd /home/zhangjikai/source/git/gitbook/zhangjk/parallel
-gitbook build
 cd /home/zhangjikai/source/git/github/parallel
 git checkout master
 git pull
 git checkout gh-pages
 git pull
+cd /home/zhangjikai/source/git/gitbook/zhangjk/parallel
+gitbook build
 yes | cp -rf /home/zhangjikai/source/git/gitbook/zhangjk/parallel/_book/* /home/zhangjikai/source/git/github/parallel/
+cd /home/zhangjikai/source/git/github/parallel
+git checkout gh-pages
 git add -A .
 git commit -m "update"
 git push
 git checkout master
-rsync -av --exclude='_book' --exclude='.git' --exclude='node_modules' -exclude='README.md' . /home/zhangjikai/source/git/github/parallel/
+rsync -av --exclude='_book' --exclude='.git' --exclude='node_modules' -exclude='README.md' /home/zhangjikai/source/git/gitbook/zhangjk/parallel/ /home/zhangjikai/source/git/github/parallel/
 git add -A .
 git commit -m "update"
